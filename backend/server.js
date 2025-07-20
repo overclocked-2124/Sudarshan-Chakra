0de30d1aa50665515dce1809450f50df2eee7e0b
snehal <snehalreddy@gmail.com>
2025-07-20 17:08:23 +0530

    small commit

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()) 
 
 // MongoDB connection
 const MONGODB_URI = "mongodb+srv://snehalreddy:S0OcbrCRXJmAZrAd@sudarshan-chakra-cluste.0hokvj0.mongodb.net/radarDB";
